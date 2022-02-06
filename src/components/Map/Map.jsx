5e668b2aa2dfd0b948ce3f0a088863ef51a617cc
Victor Fedorov <vpfedorov25@gmail.com>
2022-02-06 17:29:13 +0300

    bootstrapURLKey fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -16,6 +16,7 @@ const Map = () => {
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
